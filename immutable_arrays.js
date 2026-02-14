34baa06d97756992f4e06968a2b4a4d102ec9d22
Piranha <nanaroselynn@gmail.com>
2026-02-14 11:34:19 +0100

    Erweiterte User

--- immutable_arrays.js
+++ immutable_arrays.js
@@ -3,3 +3,9 @@ const namen = [
     { id: 2, user: "Thivya", rolle: "gugus" }
 ]; 
 console.log("Initial Array:", namen); 
+
+const neuerUser = { id: 3, user: "Perris", rolle: "Kakadou" };
+const usersErweitert = [...namen, neuerUser]; 
+
+console.log("Altes Array nicht verändert:", namen);
+console.log("Neues Array mit 3 Usern:", usersErweitert); 
