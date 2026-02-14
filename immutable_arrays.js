720adafb1da99f4024d9878ef27da425206ce548
Piranha <nanaroselynn@gmail.com>
2026-02-14 11:41:03 +0100

    map

--- immutable_arrays.js
+++ immutable_arrays.js
@@ -9,3 +9,11 @@ const usersErweitert = [...namen, neuerUser];
 
 console.log("Altes Array nicht verändert:", namen);
 console.log("Neues Array mit 3 Usern:", usersErweitert); 
+
+const nameGeaendert = usersErweitert.map(u => {
+    if (u.id ===2) {
+        return { ...u, rolle: "Hero"};
+    }
+});
+console.log("Vorher: Thivya ist gugus:", usersErweitert[1]);
+ console.log("Nacher: Thivya ist Hero:", nameGeaendert[1]);
