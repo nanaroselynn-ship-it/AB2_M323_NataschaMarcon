eaa4b7ef632feb9d7bbfeca63f069241cccbedf4
Piranha <nanaroselynn@gmail.com>
2026-02-14 11:47:06 +0100

    filter

--- immutable_arrays.js
+++ immutable_arrays.js
@@ -11,9 +11,15 @@ console.log("Altes Array nicht verändert:", namen);
 console.log("Neues Array mit 3 Usern:", usersErweitert); 
 
 const nameGeaendert = usersErweitert.map(u => {
-    if (u.id ===2) {
+    if (u.id === 2) {
         return { ...u, rolle: "Hero"};
     }
+    return u;
 });
 console.log("Vorher: Thivya ist gugus:", usersErweitert[1]);
  console.log("Nacher: Thivya ist Hero:", nameGeaendert[1]);
+
+ const usersOhneKleinsteId = nameGeaendert.filter(u => u.id !== 1);
+ 
+ console.log("vorher mit ID1:", nameGeaendert); 
+ console.log("Nacher ID 1 entfernt:", usersOhneKleinsteId); 
